a2598411f200d01c4af559ca8f304207115cf1f7
Carghaez <kejan73@gmail.com>
2017-11-16 10:48:37 +0100

    test

--- src/Auth/Middleware/AccessTokenChecker.php
+++ src/Auth/Middleware/AccessTokenChecker.php
@@ -53,7 +53,7 @@ class AccessTokenChecker
     {
         if ($this->app->environment() !== 'testing') {
             try {
-                return $this->auth->handle($request, $next, 'api');
+                return $this->auth->handle($request, $next, 'auth:api');
             } catch (AuthenticationException $e) {
                 throw new UnauthorizedHttpException('Challenge');
             }
